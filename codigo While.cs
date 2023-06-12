de197e3ee3d397998c0d985f0aa63348487aaad0
EwersonNeias <128439222+EwersonNeias@users.noreply.github.com>
2023-06-12 19:28:27 -0300

    Update codigo While.cs

--- codigo While.cs
+++ codigo While.cs
@@ -1,20 +1,20 @@
 using System;
-
+//Biblioteca usada
 class Program
 {
     static void Main(string[] args)
     {
-        int num, i = 1;
+        int num, i = 1;  // Declaração das variáveis 'num' e 'i' e inicialização de 'i' como 1.
         
-        Console.Write("Digite um numero inteiro positivo: ");
-        num = int.Parse(Console.ReadLine());
+        Console.Write("Digite um numero inteiro positivo: ");  // Solicita ao usuário que digite um número inteiro positivo.
+        num = int.Parse(Console.ReadLine());  // Lê a entrada do usuário como uma string e converte para um valor inteiro, atribuindo à variável 'num'.
         
-        while (i <= num)
+        while (i <= num)  // Enquanto 'i' for menor ou igual ao valor de 'num'.
         {
-            Console.Write(i + " ");
-            i++;
+            Console.Write(i + " ");  // Imprime o valor de 'i' seguido de um espaço em branco.
+            i++;  // Incrementa o valor de 'i' em 1.
         }
         
-        Console.WriteLine();
+        Console.WriteLine();  // Imprime uma nova linha após o loop while.
     }
 }
