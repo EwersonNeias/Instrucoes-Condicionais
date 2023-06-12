f9b81312878f841e197cb9e769937e3a7d3eb057
EwersonNeias <128439222+EwersonNeias@users.noreply.github.com>
2023-06-12 19:35:53 -0300

    Add files via upload

---
+++ For.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero;
+        
+        for (numero = 1; numero <= 10; numero++)
+        {
+            Console.WriteLine("Numero ...: " + numero);
+        }
+    }
+}
