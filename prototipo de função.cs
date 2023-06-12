0a738c2e221eb0ce02c484def17cbe721155d5b8
EwersonNeias <128439222+EwersonNeias@users.noreply.github.com>
2023-06-12 19:34:58 -0300

    Add files via upload

---
+++ prototipo de função.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program
+{
+    static int Soma(int a, int b)
+    {
+        int resultado = a + b;
+        return resultado;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Soma(2, 5));
+    }
+}
+
+
+
+
+
+
+
