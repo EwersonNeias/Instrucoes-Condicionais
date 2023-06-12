18cdecbc4a3a59a55e3145de2c7cccc3e96a52d6
EwersonNeias <128439222+EwersonNeias@users.noreply.github.com>
2023-06-12 19:47:05 -0300

    Update For.cs

--- For.cs
+++ For.cs
@@ -1,14 +1,14 @@
 using System;
-
-class Program
+//Biblioteca usada no codigo
+class Program //Nomeando a classe
 {
     static void Main(string[] args)
     {
-        int numero;
+        int numero;  // Declaração da variável 'numero' para armazenar os valores de 1 a 10.
         
-        for (numero = 1; numero <= 10; numero++)
+        for (numero = 1; numero <= 10; numero++)  // Loop for que inicia em 1, executa enquanto 'numero' for menor ou igual a 10, e incrementa 'numero' em 1 a cada iteração.
         {
-            Console.WriteLine("Numero ...: " + numero);
+            Console.WriteLine("Numero ...: " + numero);  // Imprime a mensagem "Numero ...: " seguida pelo valor atual de 'numero'.
         }
     }
 }
